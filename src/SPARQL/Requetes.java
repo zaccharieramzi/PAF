2f791e92de0c80c10cd7946769eedb203064b1a5
nouha.chhih <nouha.chhih@telecom-paristech.fr>
2014-06-23 16:29:49 +0200

    Methode faire des requette avec getter de nomDeVariables et valeurDeVariables

--- src/SPARQL/Requetes.java
+++ src/SPARQL/Requetes.java
@@ -4,37 +4,17 @@ package SPARQL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.jena.atlas.io.IndentedWriter;
-import org.apache.log4j.Logger;
-
-import com.github.jsonldjava.core.RDFDataset.Literal;
 import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 
+public class Requetes {
 
-
-
-public class Requetes{
-	public static void main( String[] args ) {
+	private ArrayList<String> nomDesVariables;
+	private ArrayList<String> valeurDesVariables;
+	
+	public void executeQuery (String queryString){
 		org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
-		String queryString=
-				"PREFIX owl: <http://www.w3.org/2002/07/owl#>"+
-						"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"+
-						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
-						"PREFIX foaf: <http://xmlns.com/foaf/0.1/>" +
-						"PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
-						"PREFIX : <http://dbpedia.org/resource/>" +
-						"PREFIX dbpedia2: <http://dbpedia.org/property/>" +
-						"PREFIX dbpedia: <http://dbpedia.org/>"+
-						"PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
-						"select * where {"
-						+ "<http://dbpedia.org/resource/Nighthawks> <http://dbpedia.org/ontology/author> ?auteur;"
-						+ "<http://dbpedia.org/property/title> ?name;"
-						+ "<http://dbpedia.org/property/type> ?type;"
-						+ "<http://dbpedia.org/property/year> ?date"
-						+ "}";
-
+		
 		Query query = QueryFactory.create(queryString);
 		QueryExecution qexec = QueryExecutionFactory.sparqlService("http://www.dbpedia.org/sparql", query);
 
@@ -46,7 +26,9 @@ public class Requetes{
 			    for(int i =0; i<nomDeColonnes.size(); i++){
 			    	 RDFNode thing= row.get(nomDeColonnes.get(i));
 //					 Literal label=row.getLiteral(nomDeColonnes.get(i));
-					 System.out.println(thing.toString()+" is "+nomDeColonnes.get(i));
+			    	 nomDesVariables.add(nomDeColonnes.get(i));
+			    	 valeurDesVariables.add(thing.toString());
+					 System.out.println(thing.toString()+ " is " + " " +nomDeColonnes.get(i));
 			    }
 			   
 			}
@@ -55,6 +37,20 @@ public class Requetes{
 			qexec.close();
 		}
 	}
+
+	public ArrayList<String> getNomDesVariables() {
+		return nomDesVariables;
+	}
+
+	
+
+	public ArrayList<String> getValeurDesVariables() {
+		return valeurDesVariables;
+	}
+
+	
+	
+	
 }
             			  
          
