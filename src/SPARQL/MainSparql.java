68435b32b238bea81f18f13af5086e50e1929fc8
nouha.chhih <nouha.chhih@telecom-paristech.fr>
2014-06-25 14:56:51 +0200

    Main sparql

---
+++ src/SPARQL/MainSparql.java
@@ -0,0 +1,33 @@
+package SPARQL;
+
+public class MainSparql {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		String queryString=
+				"PREFIX owl: <http://www.w3.org/2002/07/owl#>"+
+				"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"+
+				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
+				"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+				"PREFIX foaf: <http://xmlns.com/foaf/0.1/>" +
+				"PREFIX dc: <http://purl.org/dc/elements/1.1/>" +
+				"PREFIX : <http://dbpedia.org/resource/>" +
+				"PREFIX dbpedia2: <http://dbpedia.org/property/>" +
+				"PREFIX dbpedia: <http://dbpedia.org/>"+
+				"PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
+						"select * where {"
+						+ "<http://dbpedia.org/resource/Nighthawks> <http://dbpedia.org/ontology/author> ?auteur;"
+						+ "<http://dbpedia.org/property/title> ?name;"
+						+ "<http://dbpedia.org/property/type> ?type;"
+						+ "<http://dbpedia.org/property/year> ?date"
+						+ "}";
+		
+		Requetes query = new Requetes();
+		query.executeQuery(queryString);
+		query.getNomDesVariables();
+		query.getValeurDesVariables();
+		
+	}
+
+}
